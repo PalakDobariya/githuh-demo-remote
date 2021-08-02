0e6af004ac9093796de626681958d6aaf640cdf9
Palak Dobariya <47080047+PalakDobariya@users.noreply.github.com>
2021-08-02 14:33:19 +0530

    uploaded file

---
+++ math.py
@@ -0,0 +1,13 @@
+# Add implentation
+def add(x,y):
+    return x+y
+
+#Subtract implementation
+def subtract(x,y):
+    return x-y          # on master branch
+
+def multiply(x,y):
+    pass
+
+def divide(x,y):
+    pass
